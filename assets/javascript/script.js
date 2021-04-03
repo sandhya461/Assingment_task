127c12f43165e86a39bdb26340ceab728d520727
sandhya <sandhya.rampurek@gmail.com>
2021-04-02 23:53:55 -0700

    assignment task

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
